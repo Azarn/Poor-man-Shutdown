25a5907de817b49c908e19e668f5d3c307901f43
Alexander <Alex3296@yandex.ru>
2017-04-04 14:39:15 +0300

    Add project files.

---
+++ Poor man’s Shutdown/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poor_man_s_Shutdown {
+    class Program {
+        static void Main(string[] args) {
+        }
+    }
+}
